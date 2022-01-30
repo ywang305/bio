4e8f87a7a140b767aec716a39b529a6cb9e93881
Yao Wang <ydwang@icapitalnetwork.com>
2022-01-30 01:03:31 -0500

    framework

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Chakra from "../components/chakra";
+import Navbar from "../components/navbar";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <Chakra cookies={pageProps.cookies}>
+      <Navbar path={router.pathname} />
+      <Component {...pageProps} />
+    </Chakra>
+  );
 }
 
-export default MyApp
+export default MyApp;
