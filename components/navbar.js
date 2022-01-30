7a59184a38ed15b42fd068ccc2aca33793cb6cdc
Yao Wang <ydwang@icapitalnetwork.com>
2022-01-30 01:07:19 -0500

    navbar

--- components/navbar.js
+++ components/navbar.js
@@ -79,7 +79,7 @@ const Navbar = (props) => {
           </LinkItem>
           <LinkItem
             _target="_blank"
-            href="https://github.com/craftzdog/craftzdog-homepage"
+            href="https://github.com/ywang305/bio"
             path={path}
             display="inline-flex"
             alignItems="center"
